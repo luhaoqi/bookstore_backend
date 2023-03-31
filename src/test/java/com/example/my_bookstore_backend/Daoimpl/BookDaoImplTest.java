2dc231271064c039f821f46b966a625587d5e596
Jiarui Wang <wangjiarui4@outlook.com>
2023-03-31 16:51:02 +0800

    test: BookDaoImplTest
    path cover

---
+++ src/test/java/com/example/my_bookstore_backend/Daoimpl/BookDaoImplTest.java
@@ -0,0 +1,62 @@
+package com.example.my_bookstore_backend.Daoimpl;
+
+import com.example.my_bookstore_backend.entity.Book;
+import com.example.my_bookstore_backend.repository.BookRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class BookDaoImplTest {
+    private final BookDaoImpl bookDao = new BookDaoImpl();
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        bookDao.setBookRepository(bookRepository);
+    }
+
+    @Test
+    void save() {
+        Book book = new Book();
+        bookDao.save(book);
+        verify(bookRepository).save(book);
+    }
+
+    @Test
+    void delete() {
+        Book book = new Book();
+        book.setFlag(1);
+        bookDao.delete(book);
+        Assertions.assertEquals(book.getFlag(), 0);
+        verify(bookRepository).save(book);
+    }
+
+    @Test
+    void getAllBooks() {
+        ArrayList<Book> books = new ArrayList<>();
+        when(bookRepository.findAll()).thenReturn(books);
+        Assertions.assertEquals(bookDao.getAllBooks(), books);
+    }
+
+    @Test
+    void getBookById() {
+        Book book = new Book();
+        book.setBid(0);
+        when(bookRepository.findByBid(book.getBid())).thenReturn(book);
+        Assertions.assertEquals(bookDao.getBookById(book.getBid()), book);
+        when(bookRepository.findByBid(-1)).thenReturn(null);
+        Assertions.assertNull(bookDao.getBookById(-1));
+
+    }
+}
